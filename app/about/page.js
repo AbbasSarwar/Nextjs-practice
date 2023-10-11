f9317f05da7a57fa450f7b7b6a1a7a69d374952e
Abbas Sarwar <abbassarwar100@gmail.com>
2023-10-11 19:48:17 +0500

    A page paragraph added

--- app/about/page.js
+++ app/about/page.js
@@ -2,6 +2,7 @@ const About = () => {
     return (
         <section className="flex h-screen">
             <h1>About page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis maxime sunt excepturi nulla. Est laborum vitae, suscipit sunt esse eveniet quis quos impedit voluptatum, veritatis, eos reiciendis. Nisi, modi dignissimos!</p>
         </section>
     )
 }
