d1c3a91f1c1dd8b0dd915c8958732969a4540a43
Abbas Sarwar <abbassarwar100@gmail.com>
2023-10-27 15:53:34 +0500

    paragraph added

--- app/home/page.js
+++ app/home/page.js
@@ -2,6 +2,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nihil qui sit deserunt possimus cum perspiciatis placeat, et ipsum, eaque commodi neque animi quasi eum excepturi voluptates! Nisi, deserunt dicta?</p>
         </div>
     )
 }
